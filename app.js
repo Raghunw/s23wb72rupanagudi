49386bc7634d36c1a6a28bca88fba42329dabf1b
Raghu Reddy <s561672@nwmissouri.edu>
2023-04-05 00:07:09 -0500

    Added images

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var birdsRouter = require('./routes/birds');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
+
 
 
 
@@ -25,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/birds', birdsRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
